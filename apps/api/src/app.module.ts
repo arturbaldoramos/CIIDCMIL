b9fb7e4f1ebcceafa14136e094dc7315183ff143
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2025-05-23 00:35:47 -0300

    create auth and user structure

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,16 @@
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    UserModule,
+    AuthModule,],
+  controllers: [
+    AuthController, AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
