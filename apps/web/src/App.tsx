ea480d396774cf3f1ef7c3f78f988eaf06541bdb
Nicolas Santos <nicolasps.contato@gmail.com>
2025-05-28 00:40:35 -0300

    Updated import path

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/homePage';
+import { HomePage } from './pages/HomePage';
 import { WelcomePage } from './pages/WelcomePage';
 import { ThemeProvider } from './Components/theme-provider';
 
