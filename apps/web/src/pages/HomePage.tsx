a863b7da8de97d05db2626a82915c71aaf5b9b08
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2025-06-07 19:54:26 -0300

    Updated fonts and login image

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -11,7 +11,7 @@ export function HomePage() {
   return (
     <div className="min-h-screen bg-background text-foreground flex flex-col items-center justify-center p-4 ">
       <div className=" max-w-xl mx-auto p-6">
-        <h1 className="text-[40px] leading-[45px] sm:text-5xl md:text-5xl lg:text-5xl font-semibold mb-8 ">
+        <h1 className="text-4xl sm:text-5xl md:text-5xl lg:text-5xl font-semibold mb-8 ">
           Percepção das Barreiras Culturais e de Comunicação percebidas no Website da Prefeitura de Recife
         </h1>
 
