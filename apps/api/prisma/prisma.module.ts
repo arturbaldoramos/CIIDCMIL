57465f4bf18193f44f1ac9a1789cec524fa84b3d
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2025-07-02 09:26:32 -0300

    Revert "Added api logs and prod files"
    This reverts commit 7b4b4fd6c7be4d7567f80ec84930bb88da94cc18.

--- apps/api/prisma/prisma.module.ts
+++ apps/api/prisma/prisma.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-
 @Module({
   providers: [PrismaService],
   exports: [PrismaService], // Exporta para outros módulos usarem
